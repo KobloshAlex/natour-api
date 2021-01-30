d748f8ce51489f40e76f31af55166f3c838ed8b0
Alex <Koblosh.Alex@gmail.com>
2021-01-30 17:05:27 -0500

    add

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
+
 dotenv.config({ path: "./config/config.env" });
 const app = require("./app");
 
 const port = process.env.PORT;
+
 app.listen(port, () => console.log(`App is running on port ${port}`));
