6883531bd5e466f1b6781619f2b9803cb6ec5c9b
Alex <Koblosh.Alex@gmail.com>
2021-02-02 21:51:34 -0500

    add

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,17 @@ const {
   getAllTours,
   getTourById,
   updateTour,
+  getMonthlyPlan,
+  getTourStatistics,
 } = require("../controllers/tourController");
 const router = express.Router();
 
 router.route("/").get(getAllTours).post(createTour);
+
+router.route("/tour-stats").get(getTourStatistics);
+
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
+
 router.route("/:id").get(getTourById).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
